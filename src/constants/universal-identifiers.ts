83ef8b78d94603d581da42a55766c355da9b383d
Samuel Jörblad <samuel.jorblad@sr.se>
2026-04-24 15:17:16 +0200

    Initial commit from Create Twenty App

---
+++ src/constants/universal-identifiers.ts
@@ -0,0 +1,4 @@
+export const APP_DISPLAY_NAME = 'Twenty warehousecore';
+export const APP_DESCRIPTION = 'Connects twenty to warehouse core to book jobs from twenty instead of rentalcore';
+export const APPLICATION_UNIVERSAL_IDENTIFIER = '07540ac5-3c91-4679-a506-8697dc5aab90';
+export const DEFAULT_ROLE_UNIVERSAL_IDENTIFIER = '91d8b836-f48b-439f-b3ba-f9413a3ee502';
